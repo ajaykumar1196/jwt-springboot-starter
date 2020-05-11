93a14654099fe84937d134dcd9314f6c278af85c
ajaykumar <ajaykumar1196@gmail.com>
2020-05-11 11:19:07 +0530

    Initial commit

---
+++ src/main/java/com/jwtspringbootstarter/JwtSpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.jwtspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringbootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringbootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jwtspringbootstarter/JwtSpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringbootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
