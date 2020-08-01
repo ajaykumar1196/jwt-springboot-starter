412961510fdfe0901f6bb38b627b370c72cf06d6
ajaykumar <ajaykumar1196@gmail.com>
2020-08-01 15:39:15 +0530

    Added User and Authority model files

---
+++ src/main/java/com/jwtspringbootstarter/model/Authority.java
@@ -0,0 +1,33 @@
+package com.jwtspringbootstarter.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authorities")
+public class Authority implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "authority")
+    private String authority;
+
+    @JsonIgnore
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
