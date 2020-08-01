39316fbdb5a47293f0d2333be2901393a97dc541
ajaykumar <ajaykumar1196@gmail.com>
2020-08-01 23:55:51 +0530

    Added UserDetailsServiceImpl and UserRepository

---
+++ src/main/java/com/jwtspringbootstarter/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.jwtspringbootstarter.service;
+
+import com.jwtspringbootstarter.model.User;
+import com.jwtspringbootstarter.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user " + "Found with username : " + username));
+
+        return new org.springframework.security
+                .core.userdetails.User(user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isAccountNonLocked(),
+                user.getAuthorities());
+
+    }
+}
