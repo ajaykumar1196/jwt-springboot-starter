c59857d25a1eb2cbf5ee477636937f597149ddf6
ajaykumar <ajaykumar1196@gmail.com>
2020-08-02 22:47:46 +0530

    add username already exist check

--- src/main/java/com/jwtspringbootstarter/resource/AuthController.java
+++ src/main/java/com/jwtspringbootstarter/resource/AuthController.java
@@ -4,8 +4,11 @@ import com.jwtspringbootstarter.dto.AuthenticationResponse;
 import com.jwtspringbootstarter.dto.LoginRequest;
 import com.jwtspringbootstarter.dto.RegisterRequest;
 import com.jwtspringbootstarter.service.AuthService;
+import com.jwtspringbootstarter.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.OK;
@@ -16,12 +19,20 @@ import static org.springframework.http.HttpStatus.OK;
 public class AuthController {
 
     private final AuthService authService;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+
+        UserDetails userExists = userDetailsService.loadUserByUsername(registerRequest.getUsername());
+
+        if(userExists != null){
+            return new ResponseEntity<>("There is already a user registered with the username provided", HttpStatus.BAD_REQUEST);
+        }
+
         authService.signup(registerRequest);
-        return new ResponseEntity<>("User Registration Successful",
-                OK);
+
+        return new ResponseEntity<>("User Registration Successful", OK);
     }
 
 
